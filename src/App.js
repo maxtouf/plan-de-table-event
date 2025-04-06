a37e3b94590c8e0f52defcbd35666c8a032bce10
maxtouf <140697608+maxtouf@users.noreply.github.com>
2025-04-06 18:32:42 +0200

    Ajout du fichier App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GestionnairePlanDeTable from './components/GestionnairePlanDeTable';
+
+function App() {
+  return (
+    <div className="App">
+      <GestionnairePlanDeTable />
+    </div>
+  );
+}
+
+export default App;
